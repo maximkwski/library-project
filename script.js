e42aa7876f0328848632616291e38dcdf420ca65
Maksym <makkie.makowski@gmail.com>
2023-05-18 17:39:27 -0400

    add function to display books from array

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let myLibrary = [];
+const myLibrary = [
+    {title: 'The Seven Husbands of Evelyn Hugo', author: 'Taylor Jenkins Reid', pages: 320, read: 'yes'},
+    {title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 421, read: 'not read yet'},
+    {title: 'The Alchemist', author: 'Paulo Coelho', pages: 288, read: 'not read yet'},
+];
 
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read,
-    this.info = function() {
-        return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
-    }
-}
+// function Book(title, author, pages, read) {
+//     this.title = title,
+//     this.author = author,
+//     this.pages = pages,
+//     this.read = read,
+//     this.info = function() {
+//         return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
+//     }
+// }
 
-let book1 = new Book ('In Da House', 'Player Stark', 123, 'not read yet');
-console.log(book1.info());
+// let book1 = new Book ('In Da House', 'Player Stark', 123, 'not read yet');
 
 function addBookToLibrary() {
     myLibrary.push({ title: prompt('Please enter the  title'), 
@@ -21,4 +24,43 @@ function addBookToLibrary() {
                     })
 }
 
-addBookToLibrary();
+
+function displayBooks(myLibrary) {
+    let booksContainer = document.getElementById('books-container'); // Assuming there's a container element with id 'books-container'
+  
+    // Clear any existing content in the container
+    booksContainer.innerHTML = '';
+  
+    // Loop through the array of book objects
+    for (let i = 0; i < myLibrary.length; i++) {
+      let book = myLibrary[i];
+  
+      // Create a card element for each book
+      let card = document.createElement('div');
+      card.classList.add('card');
+  
+      // Create elements for book information
+      let title = document.createElement('h2');
+      title.textContent = book.title;
+  
+      let author = document.createElement('p');
+      author.textContent = 'Author: ' + book.author;
+  
+      let pages = document.createElement('p');
+      pages.textContent = 'Pages: ' + book.pages;
+  
+      // Append book information to the card
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(pages);
+  
+      // Append the card to the books container
+      booksContainer.appendChild(card);
+    }
+  }
+
+displayBooks(myLibrary);
+  
+
+
+// addBookToLibrary();
